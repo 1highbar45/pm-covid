584698637f6c05ba0c98303856a29a5115a98943
1highbar45 <lhlvhsg@gmail.com>
2021-06-24 15:32:37 +0700

    pull code

--- button/district.js
+++ button/district.js
@@ -1,15 +1,17 @@
 function selectedLocation() {
     var selected = document.getElementById("location").value;
     if (selected == "gv") {
-        var goVap = new google.maps.LatLng(10.84712018, 106.6513029);
+        let goVap = new google.maps.LatLng(10.84712018, 106.6513029);
         map.setCenter(goVap);
         map.setZoom(15);
     } else if (selected == "bt") {
-        var binhThanh = new google.maps.LatLng(10.809900645809872, 106.69940622946883);
+        let binhThanh = new google.maps.LatLng(
+            10.809900645809872,
+            106.69940622946883
+        );
         map.setCenter(binhThanh);
         map.setZoom(15);
     }
 }
-
 
 // 10.809900645809872, 106.69940622946883
--- chart/chart.js
+++ chart/chart.js
@@ -1,123 +1,127 @@
 //google.charts.setOnLoadCallback(drawChart);
 
 function renderPieChart(locations) {
-  let distList = [];
-  distList[0] = {
-    distName: locations[0]["dist"],
-    num: 0,
-  };
-  for (const location of locations) {
-    // if it exist a specific district
-    const foundIndex = distList.findIndex(
-      (dist) => dist.distName == location["dist"]
+    let distList = [];
+    distList[0] = {
+        distName: locations[0]["dist"],
+        num: 0,
+    };
+    for (const location of locations) {
+        // if it exist a specific district
+        const foundIndex = distList.findIndex(
+            (dist) => dist.distName == location["dist"]
+        );
+        // if it exist a specific district, update increase num
+        if (foundIndex > -1) {
+            distList[foundIndex].num += 1;
+        }
+        // if does not exist, add new district obj
+        else {
+            distList.push({
+                distName: location["dist"],
+                num: 1,
+            });
+        }
+    }
+
+    // Create Schemma
+    let schemmaArr = [["Quận", "Số khu vực nhiễm"]];
+    for (const dist of distList) {
+        schemmaArr.push([dist.distName, dist.num]);
+    }
+
+    let data = google.visualization.arrayToDataTable(schemmaArr);
+
+    let options = {
+        title: "Các quận ở TP.HCM",
+        //titlePosition: "none",
+        legend: { position: "none" },
+        chartArea: {},
+        width: "50%",
+        height: 400,
+        is3D: true,
+    };
+
+    let chart = new google.visualization.PieChart(
+        document.getElementById("piechart")
     );
-    // if it exist a specific district, update increase num
-    if (foundIndex > -1) {
-      distList[foundIndex].num += 1;
-    }
-    // if does not exist, add new district obj
-    else {
-      distList.push({
-        distName: location["dist"],
-        num: 1,
-      });
-    }
-  }
 
-  // Create Schemma
-  let schemmaArr = [["Quận", "Số khu vực nhiễm"]];
-  for (const dist of distList) {
-    schemmaArr.push([dist.distName, dist.num]);
-  }
-
-  let data = google.visualization.arrayToDataTable(schemmaArr);
-
-  let options = {
-    title: "Các quận ở TP.HCM",
-    //titlePosition: "none",
-    legend: { position: "none" },
-    chartArea: {},
-    width: "50%",
-    height: 400,
-    is3D: true,
-  };
-
-  let chart = new google.visualization.PieChart(
-    document.getElementById("piechart")
-  );
-
-  chart.draw(data, options);
+    chart.draw(data, options);
 }
 
 function renderLineChart(locations) {
-  var dateList = [];
-  dateList[0] = {
-    date: locations[0].date,
-    num: 0,
-  };
- 
-  for (const location of locations) {
-    // if it exist a specific district
-    const foundIndex = dateList.findIndex(
-      (date) => date.date === location["date"]
+    var dateList = [];
+    dateList[0] = {
+        date: locations[0].date,
+        num: 0,
+    };
+
+    for (const location of locations) {
+        // if it exist a specific district
+        const foundIndex = dateList.findIndex(
+            (date) => date.date === location["date"]
+        );
+
+        // if it exist a specific district, update increase num
+        if (foundIndex > -1) {
+            dateList[foundIndex].num += 1;
+        }
+        // if does not exist, add new district obj
+        else {
+            dateList.push({
+                date: location["date"],
+                num: 1,
+            });
+        }
+    }
+
+    let data = new google.visualization.DataTable();
+    data.addColumn("date", "Ngày");
+    data.addColumn("number", "Số khu cách ly");
+
+    const dateFormat = dateList[0].date.split("/");
+    const newDate = new Date(
+        `${dateFormat[2]}-${dateFormat[1]}-${dateFormat[0]}`
+    );
+    console.log(newDate);
+    console.log(dateList[0].num);
+    let rows = [];
+    for (const element of dateList) {
+        // Take day, month ,year from date format date/month/year
+        const dateFormat = element.date.split("/");
+        rows.push([
+            new Date(`${dateFormat[2]}-${dateFormat[1]}-${dateFormat[0]}`),
+            element.num,
+        ]);
+    }
+    console.log(rows);
+    data.addRows(rows);
+
+    let options = {
+        title: "Số khu cách ly ở TP.HCM",
+        width: "50%",
+        height: 400,
+        hAxis: {
+            format: "d/M",
+            gridlines: { count: 15 },
+        },
+        vAxis: {
+            gridlines: { color: "none" },
+            minValue: 0,
+        },
+    };
+
+    // let data = google.visualization.arrayToDataTable([
+    //   ['Year', 'Sales'],
+    //   ['2004',  1000],
+    //   ['2005',  1170],
+    //   ['2006',  660 ],
+    //   ['2007',  1030]
+    // ]);
+
+    let chart = new google.visualization.LineChart(
+        document.getElementById("linechart")
     );
 
-    // if it exist a specific district, update increase num
-    if (foundIndex > -1) {
-      dateList[foundIndex].num += 1;
-    }
-    // if does not exist, add new district obj
-    else {
-      dateList.push({
-        date: location["date"],
-        num: 1,
-      });
-    }
-  }
-  
-
-  let data = new google.visualization.DataTable();
-  data.addColumn("date", "Ngày");
-  data.addColumn("number", "Số khu cách ly");
-  
-  const dateFormat = dateList[0].date.split('/')
-  const newDate = new Date(`${dateFormat[2]}-${dateFormat[1]}-${dateFormat[0]}`)
-  console.log(newDate)
-  console.log(dateList[0].num)
-  let rows = []
-  for (const element of dateList) {
-    // Take day, month ,year from date format date/month/year
-    const dateFormat = element.date.split('/')
-    rows.push([new Date(`${dateFormat[2]}-${dateFormat[1]}-${dateFormat[0]}`),element.num]) 
-  }
-  console.log(rows)
-  data.addRows(rows)
-
-  let options = {
-    title: "Số khu cách ly ở TP.HCM",
-    width: "50%",
-    height: 400,
-    hAxis: {
-      format: "d/M",
-      gridlines: { count: 15 },
-    },
-    vAxis: {
-      gridlines: { color: "none" },
-      minValue: 0,
-    },
-  };
-
-  // let data = google.visualization.arrayToDataTable([
-  //   ['Year', 'Sales'],
-  //   ['2004',  1000],
-  //   ['2005',  1170],
-  //   ['2006',  660 ],
-  //   ['2007',  1030]
-  // ]);
-
-  let chart = new google.visualization.LineChart(
-    document.getElementById("linechart")
-  );
-
-  chart.draw(data, options);
+    chart.draw(data, options);
 }
