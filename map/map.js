41fff263798ae7fe863cca0808011c4a97f37465
1highbar45 <lhlvhsg@gmail.com>
2021-06-24 10:24:38 +0700

    fix conflict

--- map/map.js
+++ map/map.js
@@ -1,58 +1,57 @@
 let map, heatmap;
 function initAutocomplete() {
-  // create google map
-  // load map at defined lat and lng
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: 10.76347152,
-      lng: 106.6826186,
-    },
-    zoom: 15,
-    mapTypeId: "terrain",
-  });
+    // create google map
+    // load map at defined lat and lng
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 10.76347152,
+            lng: 106.6826186,
+        },
+        zoom: 15,
+        mapTypeId: "terrain",
+    });
 
-  //------------------API call (not yet used)---------------
-  getLocations()
-    .then((response) => {
-      const data = response;
+    //------------------API call (not yet used)---------------
+    getLocations()
+        .then((response) => {
+            const data = response;
 
-      let locations = [];
-      data.map((location) => {
-        locations = [
-          ...locations,
-          {
-            id: location[0],
-            case_id: location[1],
-            address: location[2],
-            ward: location[3],
-            dist: location[4],
-            date: location[5],
-            lat: location[6],
-            lng: location[7],
-          },
-        ];
-      }, 1);
-      locations = locations.slice(1);
-      console.log(locations);
+            let locations = [];
+            data.map((location) => {
+                locations = [
+                    ...locations,
+                    {
+                        id: location[0],
+                        case_id: location[1],
+                        address: location[2],
+                        ward: location[3],
+                        dist: location[4],
+                        date: location[5],
+                        lat: location[6],
+                        lng: location[7],
+                    },
+                ];
+            }, 1);
+            locations = locations.slice(1);
+            console.log(locations);
 
-      // Render newest location on side bar
-      renderSideBarNew(locations);
+            // Render newest location on side bar
+            renderSideBarNew(locations);
 
-      // Render locations as Markers
-      renderMapMarker(locations);
-      // Render locations as heat point (heat map layer)
-      renderHeatMapLayer(locations);
+            // Render locations as Markers
+            renderMapMarker(locations);
+            // Render locations as heat point (heat map layer)
+            renderHeatMapLayer(locations);
 
-      // Render newest location on side bar
-      renderSideBarNew(locations);
+            // Render newest location on side bar
+            renderSideBarNew(locations);
 
-      // Render heat map layer
-      renderHeatMapLayer(locations);
+            // Render heat map layer
+            renderHeatMapLayer(locations);
 
-      // Render search box output
-      renderSearchBox();
-    })
-    .catch((e) => console.log(e));
-  //------------------API call (not yet used)---------------
-
+            // Render search box output
+            renderSearchBox();
+        })
+        .catch((e) => console.log(e));
+    //------------------API call (not yet used)---------------
 }
