da25c92acc674665dba89144785515c8af56601f
1highbar45 <lhlvhsg@gmail.com>
2021-06-30 20:30:48 +0700

    hover tag

--- view/my_view/historybar.js
+++ view/my_view/historybar.js
@@ -1,29 +1,27 @@
-let counter = 0
-function renderHistoryRows (locations) {
-    const max = counter + 5
+let counter = 0;
+function renderHistoryRows(locations) {
+    const max = counter + 5;
 
-    const historyTable = document.getElementById('history-table')
-    const historyTableBody = document.getElementById('history-tbody')
-    const loadingSpinner = document.getElementById('history-loading')
+    const historyTable = document.getElementById("history-table");
+    const historyTableBody = document.getElementById("history-tbody");
+    const loadingSpinner = document.getElementById("history-loading");
 
     // Hide spinner
-    loadingSpinner.style.visibility = 'hidden'
+    loadingSpinner.style.visibility = "hidden";
 
     // Display the table
-    historyTable.style.visibility = 'visible'
+    historyTable.style.visibility = "visible";
 
-
-    while (counter < max){
-      let newRow = `<tr>
-      <th scope="row">${locations[counter]['id']}</th>
-      <td>${locations[counter]['date']}</td>
-      <td>${locations[counter]['address']}</td>
-      <td>${locations[counter]['dist']}</td>
-    </tr>`
-      let rows = historyTableBody.innerHTML
-      rows = rows + newRow
-      historyTableBody.innerHTML = rows
-      counter = counter + 1
-
+    while (counter < max) {
+        let newRow = `<tr class='history-item'>
+      <th scope="row">${locations[counter]["id"]}</th>
+      <td>${locations[counter]["date"]}</td>
+      <td>${locations[counter]["address"]}</td>
+      <td>${locations[counter]["dist"]}</td>
+    </tr>`;
+        let rows = historyTableBody.innerHTML;
+        rows = rows + newRow;
+        historyTableBody.innerHTML = rows;
+        counter = counter + 1;
     }
 }
--- view/my_view/sidebar.js
+++ view/my_view/sidebar.js
@@ -2,13 +2,13 @@
 function renderSideBarNew(locations) {
     const sidebar_new = document.getElementById("side-bar-new");
     const loadingSidebar = document.getElementById("loading-sidebar");
-    loadingSidebar.style.visibility = 'hidden'
-   
+    loadingSidebar.style.visibility = "hidden";
+
     let locations_newest = locations.slice(0, 7);
     let locations_newest_display = ``;
 
     // Clear the side bar
-    sidebar_new.innerHTML = '' 
+    sidebar_new.innerHTML = "";
     for (const location of locations_newest) {
         locations_newest_display += `<div class="sidebar-item alert alert-primary">
         <span class="badge badge-danger">Q.${location.dist}</span>  
